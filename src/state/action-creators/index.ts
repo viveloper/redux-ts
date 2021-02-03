d25bf7c9671428bbe59ff2b06f55683f79a9f191
viveloper <ucsik1234@gmail.com>
2021-02-03 10:39:21 +0900

    Adding Action Creator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from 'axios';
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+export const searchRepositories = (term: string) => async (
+  dispatch: Dispatch<Action>
+) => {
+  dispatch({ type: ActionType.SEARCH_REPOSITORIES });
+  try {
+    const { data } = await axios.get(`http://registry.npmjs.org/-/v1/search`, {
+      params: {
+        text: term,
+      },
+    });
+
+    const names = data.objects.map((result: any) => result.package.name);
+
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+      payload: names,
+    });
+  } catch (err) {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES_ERROR,
+      payload: err.message,
+    });
+  }
+};
